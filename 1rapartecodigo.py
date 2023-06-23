a527176cf7cfca7f5a8a7f0e0c48206f6feae589
agusting98 <133926923+agusting98@users.noreply.github.com>
2023-06-23 12:27:13 -0300

    Borrar Normativa
    Esta funcion, busca eliminar la normativa desde la base de datos.

--- 1rapartecodigo.py
+++ 1rapartecodigo.py
@@ -1,15 +1,11 @@
-import sqlite3
+    def delete_normativa(self, normativa_id):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+            DELETE FROM Normativa
+            WHERE id_Normativa = ?
+        """, (normativa_id,))
+        self.connection.commit()
+        print("\033[;32m"+"La normativa ha sido eliminada exitosamente.")
 
-
-class Normativa:
-    def __init__(self, id_normativa, tipo_normativa, nro_normativa, fecha, nombre, descripcion, categoria, jurisdiccion, organo_legislativo, palabras_clave):
-        self.id_normativa = id_normativa
-        self.tipo_normativa = tipo_normativa
-        self.nro_normativa = nro_normativa
-        self.fecha = fecha
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.categoria = categoria
-        self.jurisdiccion = jurisdiccion
-        self.organo_legislativo = organo_legislativo
-        self.palabras_clave = palabras_clave
+##Nombre de commit: Borrar Normativa
+##Descripcion de commit: Esta funcion, busca eliminar la normativa desde la base de datos.
