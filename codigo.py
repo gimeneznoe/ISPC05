7053cb3526e905ed24eba8b69fc524baa1f2c369
Silvana Godoy <112569467+SilvanaAGodoy@users.noreply.github.com>
2023-06-23 15:40:10 -0300

    Insertar/Actualizar
    Se insertan o actualizan las normativas existentes

---
+++ codigo.py
@@ -0,0 +1,48 @@
+    def insert_normativa(self, tipo_normativa, nro_normativa, fecha, nombre, descripcion, categoria, jurisdiccion, organo_legislativo, palabras_clave):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+            INSERT INTO Normativa (TipoNormativa, NroNormativa, Fecha, Nombre, Descripcion, Categoria, Jurisdiccion, OrganoLegislativo, PalabrasClave)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (tipo_normativa, nro_normativa, fecha, nombre, descripcion, categoria, jurisdiccion, organo_legislativo, palabras_clave))
+        self.connection.commit()
+        print("\033[;32m"+"La normativa ha sido insertada exitosamente.")
+
+
+    def update_normativa(self, normativa_id):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+            SELECT *
+            FROM Normativa
+            WHERE id_Normativa = ?
+        """, (normativa_id,))
+        normativa_data = cursor.fetchone()
+
+        if normativa_data:
+            Normativa(*normativa_data)
+            tipo_normativa = input("Ingrese el nuevo tipo de normativa: ")
+            nro_normativa = input("Ingrese el nuevo número de normativa: ")
+            fecha = input("Ingrese la nueva fecha de la normativa: ")
+            nombre = input("Ingrese el nuevo nombre de la normativa: ")
+            descripcion = input(
+                "Ingrese la nueva descripción de la normativa: ")
+            categoria = input("Ingrese la nueva categoría de la normativa: ")
+            jurisdiccion = input(
+                "Ingrese la nueva jurisdicción de la normativa: ")
+            organo_legislativo = input(
+                "Ingrese el nuevo órgano legislativo de la normativa: ")
+            palabras_clave = input(
+                "Ingrese las nuevas palabras clave de la normativa (separadas por comas): ")
+
+            cursor.execute("""
+                UPDATE Normativa
+                SET TipoNormativa = ?, NroNormativa = ?, Fecha = ?, Nombre = ?, Descripcion = ?, Categoria = ?, Jurisdiccion = ?, OrganoLegislativo = ?, PalabrasClave = ?
+                WHERE id_Normativa = ?
+            """, (tipo_normativa, nro_normativa, fecha, nombre, descripcion, categoria, jurisdiccion, organo_legislativo, palabras_clave, normativa_id))
+            self.connection.commit()
+            print("\033[;32m"+"La normativa ha sido actualizada exitosamente.")
+        else:
+            print("\033[;31m" +
+                  "No se encontró la normativa con el ID especificado.")
+
+# Nombre del commit: Insertar/Actualizar
+# Descripcion del commit: Se insertan o actualizan las normativas existentes
