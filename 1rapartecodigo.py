35f415f42279bbf002b75e5a36c65b9a0c3e0cbb
agusting98 <133926923+agusting98@users.noreply.github.com>
2023-06-23 13:17:07 -0300

    Delete 1rapartecodigo.py

--- 1rapartecodigo.py
+++
@@ -1,8 +0,0 @@
-    def delete_normativa(self, normativa_id):
-        cursor = self.connection.cursor()
-        cursor.execute("""
-            DELETE FROM Normativa
-            WHERE id_Normativa = ?
-        """, (normativa_id,))
-        self.connection.commit()
-        print("\033[;32m"+"La normativa ha sido eliminada exitosamente.")
